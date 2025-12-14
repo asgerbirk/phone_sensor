a54c1aa5af12b6617edc452e73705c4400ed70fa
asgerbirk <asger.birk@gmail.com>
2025-12-14 17:11:11 +0100

    final

--- receiver.js
+++ receiver.js
@@ -4,5 +4,5 @@ function modtager(topic, modtagetBesked) {
   let afsender = modtagerBuffer.from;
   console.log("Modtager buffer" + modtagerBuffer.from);
   let value = modtagerBuffer.val;
-  console.log("MODTAGET fra " + afsender + ": " + value); // <-- Ændret  // gør noget med value
+  console.log("MODTAGET fra " + afsender + ": " + value);
 }
